2cdef7226bd151e0ca400ba11fd1e8772712a17e
Oleksandr Gnatyshyn <gnat86@gmail.com>
2014-11-10 13:55:11 +0100

    initial code commit

---
+++ Podfile
@@ -0,0 +1,5 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, "8.0"
+
+pod 'EZAudio', '~> 0.0.4'
+pod 'JSQMessagesViewController'
---
+++ audioChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  audioChat
+//
+//  Created by Oleksandr Gnatisin on 10.11.14.
+//  Copyright (c) 2014 Oleksandr Gnatisin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
